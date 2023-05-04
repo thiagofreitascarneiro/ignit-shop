be4c0cff6516b9a11cb59b9e58586b422c4c5538
thiago <thiagofreitascarneiro>
2023-05-04 16:29:43 -0300

    creating homepage

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,10 +1,24 @@
 
 import { globalStyles } from '@/styles/global';
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+
+import logoImg from '../assets/logo.svg'
+import { Container, Header } from '@/styles/pages/app';
 
 globalStyles();
 
 export default function App({ Component, pageProps }: AppProps) {
+  
 
-  return <Component {...pageProps} />
+  return (
+      <Container>
+        <Header>
+            <img src={logoImg.src} alt=""/>
+        </Header>
+
+        <Component {...pageProps} />
+      </Container>
+    )
+
+  
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,31 @@
-import { styled } from "../styles"
+import { HomeContainer, Product } from "@/styles/pages/home";
 
-const Button = styled('button', {
-  backgroundColor: '$green300',
-  borderRadius: 4,
-  border: 0,
-  padding: '4px 8px',
+import camiseta1 from '../assets/camisetas/1.png';
+import camiseta2 from '../assets/camisetas/2.png';
+import camiseta3 from '../assets/camisetas/3.png';
 
- ' &:hover': {
-    filter: 'brightbess(0.8)'
- }
+import Image from 'next/image';
 
-})
 
 export default function Home() {
   return (
-      <Button>enviar</Button>
+      <HomeContainer>
+        <Product>
+          <Image src={camiseta2} width={520} height={400} alt={""}/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+        <Product>
+          <Image src={camiseta2} width={520} height={400} alt={""}/>
+
+          <footer>
+            <strong>Camiseta X</strong>
+            <span>R$ 79,90</span>
+          </footer>
+        </Product>
+      </HomeContainer>
   )
 }
