c0c8f76398c17de5831401210ec4bfe6ce65d813
Thiago <thiagofreitascarneiro@hotmail.com>
2023-05-09 16:36:35 -0300

    creating styled page product

--- src/pages/product/[id].tsx
+++ src/pages/product/[id].tsx
@@ -1,3 +1,4 @@
+import { ImageContainer, ProductContainer, ProductDetails } from '@/styles/pages/product';
 import { useRouter } from 'next/router';
 
 export default function Product() {
@@ -5,6 +6,22 @@ export default function Product() {
     const { query } = useRouter();
 
     return (
-        <h1>Product:</h1>
+        <ProductContainer>
+            <ImageContainer>
+                
+            </ImageContainer>
+            <ProductDetails>
+                <h1>camiseta X</h1>
+                <span>R$ 79,90</span>
+
+                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. 
+                    Dolore alias natus quisquam eius aliquid asperiores, 
+                    inventore deserunt aperiam quia est
+                     totam quo mollitia molestias tempore ratione nam quae nihil quos?
+                </p>
+
+                <button>Comprar agora</button>
+            </ProductDetails>
+        </ProductContainer>
     )
 }
